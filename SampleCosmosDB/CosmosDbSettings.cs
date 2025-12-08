89c0f0466fbd79d9e9deca2a6478b5cf263b57eb
Shirish Deshmukh <132877353+shirish-d@users.noreply.github.com>
2025-12-08 18:35:36 +0530

    formatting

--- SampleCosmosDB/CosmosDbSettings.cs
+++ SampleCosmosDB/CosmosDbSettings.cs
@@ -2,10 +2,11 @@
 
 class CosmosDbSettings
 {
-	public string ResourceGroup { get; set; } = string.Empty;
 	public string AccountName { get; set; } = string.Empty;
-	public string DatabaseId { get; set; } = string.Empty;
-	public string ContainerId { get; set; } = string.Empty;
 	public string AzureClientId { get; set; } = string.Empty;
+	public string ContainerId { get; set; } = string.Empty;
+	public string DatabaseId { get; set; } = string.Empty;
+	public bool EnableManagedIdentity { get; set; }
+	public string ResourceGroup { get; set; } = string.Empty;
 	public string SubscriptionId { get; set; } = string.Empty;
 }
